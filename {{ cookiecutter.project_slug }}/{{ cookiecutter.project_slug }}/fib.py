f0ec642752da09d03323c9818a5f7557189b9849
LukasFT <github@lftm.org>
2025-01-22 16:43:14 +0100

    Moved files

---
+++ {{ cookiecutter.project_slug }}/{{ cookiecutter.project_slug }}/fib.py
@@ -0,0 +1,15 @@
+from functools import cache
+
+
+@cache  # Enable top-down memoization (Dynamic Programming)
+def fib(n: int):
+    """Return the n-th Fibonacci number."""
+    if n < 0:
+        raise ValueError("n must be a non-negative integer.")
+
+    if n == 0:
+        return 0
+    if n == 1:
+        return 1
+
+    return fib(n - 1) + fib(n - 2)
